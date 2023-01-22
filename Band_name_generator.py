05b9840b5468c94e3e8b09e163e199f0a22f8c6e
Asmita Ojha <asmitaojha05@gmail.com>
2023-01-22 13:17:54 +0545

    Project made in first day of 100 day python course

---
+++ Band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Brand Name Generator\n")
+City= input("Which city did you grow up? \n")
+Pet_name = input("What is your pet name?\n")
+print("You band name could be: "+ City +" "+Pet_name+"\n");
